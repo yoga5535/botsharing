379fe098ce77076cdee3f4a60a56138873db9d6d
yoga5535 <77037268+yoga5535@users.noreply.github.com>
2023-01-12 18:25:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://GITHUB-USERNAME:token@github.com/GITHUB-USERNAME/YOUR-REPO-NAME okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://botsharing: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@github.com/yoga5535/filestore okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
